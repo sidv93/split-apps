0076b764df38f6bac9d3668e07ae15df6b3b6015
Siddharth Venkatesh <32375112+sidv93@users.noreply.github.com>
2020-04-03 00:43:29 +0530

    added react app

---
+++ apps/react-acp-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import AcpSidenav from '@sidv93/react-acp-sidenav';
+
+function App() {
+  return (
+    <div>
+      <AcpSidenav unreadCount={4} />
+    </div>
+  );
+}
+
+export default App;
